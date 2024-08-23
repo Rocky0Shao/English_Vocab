77fdf3c663291b5c2ba24d7407fea47e28ef0a31
Rocky Shao <rockyshao22@gmail.com>
2024-08-23 10:14:05 -0400

    asd

---
+++ Vocab_List.py
@@ -0,0 +1,108 @@
+
+vocab_list = {
+    "Diminish" : "less",
+    "Encompass" : "hold, cover",
+    "Concede": "admits",
+    "Declarative clause": " 陈述性从句",
+    "interrogative clause": "疑问从句",
+    "tenuous": "脆弱的",
+    "contentious": "有争议的",
+    "conspicuous": "outstanding, 显眼的",
+    "repudiation": "decline, 反对",
+    "intricate" : "complex, 错综复杂的",
+    "ornamentation ":"装饰",
+    "reminiscent": "令人怀念的",
+    "manuscript":"手稿",
+    "meticulously":"精心设计的",
+    "redress":"矫正",
+    "cohort":"队列",
+    "Exacerbate":"加剧",
+    "Epitomize":"缩影，概括",
+    "Preclude":"排除",
+    "Dispute":"争论",
+    "Multitude":"民众",
+    "Festive":"喜庆的",
+    "Asserting possession of":"主张拥有",
+    "Impede":"受阻",
+    "Zenith":"顶点,极盛",
+    "Espousing":"拥护",
+    "Outspoken":"直言不讳的",
+    "low-profile":"低调的",
+    "averse":"厌恶",
+    "Antropogenic":"人为的",
+    "Permeate":"渗透",
+    "Impervious":"不透水的",
+    "Aquifer":"含水层",
+    "Captive":"俘虏",
+    "Inadvertently":"不经意间",
+    "Cognitive":"认知的",
+    "Carcasses":"尸体",
+    "Gnawing":"啃咬",
+    "Cuisine":"美食",
+    "Epitomize":"缩影",
+    "Keenly":"敏锐的",
+    "Finite":"有限的",
+    "In hindsight, ...":"事后诸葛亮",
+    "Moreover":"而且",
+    "Paradigm":"Example, 范例",
+    "decisive":"决定性",
+    "dormant":"休眠的",
+    "confined to":"受限于",
+    "repressed by ...":"受...压制",
+    "prevalent":"流行的",
+    "concede":"承认",
+    "a criticism leveled against":"对...的批评",
+    "grand":"盛大的",
+    "Edifice":"建筑物",
+    "trustee":"受托人",
+    "contemporary":"当代的",
+    "depict":"描绘",
+    "an apt assesment":"恰当的评价",
+    "disparity":"差异",
+    "archaeologist":"考古学家",
+    "fragments":"碎片",
+    "colon":":",
+    "germination":"发芽",
+    "quota":"配额",
+       
+
+
+    
+
+}
+
+Grammer_lsit = { 
+    1: "No puctuation between title and the person's name"
+}
+
+
+Math = {
+
+    '''
+    Prism: 棱柱
+
+    positive difference between the ranges of number:
+        num_a = range_a_max - range_a_min
+        num_b = range_b_max - range_b_min
+        output = num_a - num_b
+    '''
+}
+
+'''
+African American Percy Julian was a scientist and entrepreneur whose work helped people around the world to see. 
+Named in 1999 as one of the greatest achievements by a US chemist in the past hundred years,
+
+
+______led to the first mass-produced treatment for glaucoma.
+A.
+Julian synthesized the alkaloid physostigmine in 1935; it 
+
+B.
+in 1935 Julian synthesized the alkaloid physostigmine, which
+
+C.
+Julian’s 1935 synthesis of the alkaloid physostigmine
+
+D.
+the alkaloid physostigmine was synthesized by Julian in 1935 and
+'''
